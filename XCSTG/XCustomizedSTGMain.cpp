48012727c4430f8e7d042b890db2ed61b497d839
LT_lrsoft <lrsoft@hotmail.com>
2019-05-23 17:26:14 +0800

    v0.61

--- XCSTG/XCustomizedSTGMain.cpp
+++ XCSTG/XCustomizedSTGMain.cpp
@@ -1,11 +1,32 @@
 #include "util/AppFrameWork.h"
+#include <mutex>
 #include <iostream>
 #include <thread>
 #ifndef _DEBUG
 #pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 #endif
-void main() {
-	xc_ogl::AppFrameWork app(720, 730, "XCDSTG_TEST_2");
+int exit_code = 0;
+std::mutex mutex;
+void SetExitType() {
+	mutex.lock();
+	exit_code++;
+	mutex.unlock();
+}
+int GetExitType() {
+	mutex.lock();
+	int code = exit_code;
+	mutex.unlock();
+	return code;
+}
+void RenderThead() {
+	xc_ogl::AppFrameWork app(720, 730, "XCDSTG v0.61pre");
 	app.init();
 	app.message_loop();
+	SetExitType();
+}
+int main() {
+	std::thread oglThread(RenderThead);
+	oglThread.detach();
+	while (!GetExitType());
+	return 0;
 }
